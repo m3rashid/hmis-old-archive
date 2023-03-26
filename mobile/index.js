9709c927b673ee4ba9e24a19ac27a115b3441fc2
m3rashid <m3rashid.hussain@gmail.com>
2023-03-27 00:46:59 +0530

    init clients

--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './app';
-import { name as appName } from './app.json';
+import App from './src/App';
+import { name as appName } from './src/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
